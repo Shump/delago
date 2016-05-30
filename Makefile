866858b5100045739e9921f35b74f49b1c66f7f0
Julian Lindblad <JulianLindblad@gmail.com>
2016-05-30 20:33:44 +0900

    Basic implementation of Digits

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	elm make src/Digits.elm --output build/Digits.js
---
+++ src/Digits.elm
@@ -0,0 +1,70 @@
+module Digits exposing (..)
+
+import String
+import List
+import List.Extra exposing (elemIndex, unfoldr, getAt)
+import Maybe.Extra exposing (traverse, combine)
+
+type Digits = Digits Int (List Int)
+
+toString : String -> Digits -> String
+toString alpha digits =
+  let
+    alpha' = String.toList alpha
+    base = String.length alpha
+    (Digits _ digs) = toBase base digits
+    chars = List.map (\d -> getAt d alpha') digs
+  in
+    Maybe.withDefault "" <| Maybe.map String.fromList <| combine chars
+
+toInt : Digits -> Int
+toInt (Digits base digs) =
+  let
+    acc dig (pow, val) = (pow + 1, val + (base ^ pow) * dig)
+  in
+    snd <| List.foldr acc (0, 0) digs
+
+toBin : Digits -> String
+toBin digits =
+  toString "01" <| toBase 2 digits
+
+toHex : Digits -> String
+toHex digits =
+  toString "0123456789ABCDEF" <| toBase 16 digits
+
+toBase' : Int -> Int -> Digits
+toBase' base num =
+  let
+    f n =
+      case n of
+        0 -> Nothing
+        _ -> Just (n `rem` base, n // base)
+  in
+    Digits base <| List.reverse <| unfoldr f num
+
+toBase : Int -> Digits -> Digits
+toBase base digits =
+  toBase' base (toInt digits)
+
+fromString : String -> Int -> String -> Maybe Digits
+fromString alphabet base string =
+  let
+    alpha = String.toList alphabet
+    chars = String.toList string
+    f c = elemIndex c alpha
+    encoded = traverse f chars
+  in
+    case encoded of
+      Nothing -> Nothing
+      Just digits -> Just <| Digits base digits
+
+fromInt : Int -> Digits
+fromInt num = toBase' 10 num
+
+fromHex : String -> Maybe Digits
+fromHex string =
+  fromString "0123456789ABCDEF" 16 <| String.toUpper string
+
+fromBin : String -> Maybe Digits
+fromBin string =
+  fromString "01" 2 string
